1dd8e5318d8be8374255add4f24d561c2a8af3af
Steve Rowe <thornkin@hotmail.com>
2015-06-04 00:59:46 -0700

    Cleaned files with limited date range

--- cleaning.r
+++ cleaning.r
@@ -1,6 +1,12 @@
 library(dplyr)
 library(lubridate)
 library(readr)
-household_power_consumption <- read_csv2("C:/Users/Steve/Desktop/tmp/Exploratory/Proj1/household_power_consumption.txt", na="?")
 
-power <- mutate(household_power_consumption, Date = dmy(Date), Time = hms(Time))
+#assumes the power consumption file is in the working directory
+#original source: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+household_power_consumption <- read_csv2("household_power_consumption.txt", na="?")
+
+power <- filter(household_power_consumption, Date == "2/1/2007" | Date == "2/2/2007")
+
+power <- mutate(power, Date = dmy(Date), Time = hms(Time))

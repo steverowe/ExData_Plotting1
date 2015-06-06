9e3ce2b10634687274a6eb62b831b57ae659b928
Steve Rowe <STROWE@microsoft.com>
2015-06-06 09:20:09 -0700

    Plots 1 and 2 are done.

--- cleaning.r
+++ cleaning.r
@@ -5,8 +5,9 @@ library(readr)
 #assumes the power consumption file is in the working directory
 #original source: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 
-household_power_consumption <- read_csv2("household_power_consumption.txt", na="?")
+power <- read_csv2("household_power_consumption.txt", na="?")
 
-power <- filter(household_power_consumption, Date == "2/1/2007" | Date == "2/2/2007")
+power <- filter(household_power_consumption, Date == "1/2/2007" | Date == "2/2/2007")
 
-power <- mutate(power, Date = dmy(Date), Time = hms(Time))
+power <- mutate(power, date.time = dmy_hms(paste(Date, Time)), Date = dmy(Date), Time = hms(Time) )
+
